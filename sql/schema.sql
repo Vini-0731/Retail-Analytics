5a528dbd0320acc369acdeb574d863a83fe88ed7
Vineeta Singh <vs07312005@gmail.com>
2026-07-09 17:16:39 +0530

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -18,3 +18,33 @@ CREATE TABLE stores (
     store_id VARCHAR(10),
     region VARCHAR(20)
 );
+
+-- 3. Products table
+CREATE TABLE products (
+    product_id VARCHAR(10) ,
+    category VARCHAR(30),
+    price DECIMAL(10,2),
+    competitor_price DECIMAL(10,2)
+);
+
+-- 4. Inventory fact table
+CREATE TABLE inventory (
+    inventory_id INT PRIMARY KEY,
+    dates DATE,
+    store_id VARCHAR(10),
+    product_id VARCHAR(10),
+    inventory_level INT,
+    units_ordered INT,
+    demand_forecast DECIMAL(10,2)
+);
+
+-- 5. Sales table
+CREATE TABLE sales (
+    sales_id INT  PRIMARY KEY,
+    dates DATE,
+    store_id VARCHAR(10),
+    product_id VARCHAR(10),
+    units_sold INT,
+    discount INT
+    );
+    
